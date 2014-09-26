6ee3965871683fa9af77da714bdc4c7443dd63db
Simon Harrison <hypertexture@gmail.com>
2014-09-26 23:23:47 +0100

    Basic Geo Location

--- geo-location.php
+++ geo-location.php
@@ -2,72 +2,69 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Hackathon</title>
+	<title>Hackathon Buses</title>
 </head>
 <body>
-<div id="out">
 
-</div>
-
+	<div id="title">Show Position In Map</div>
+	<div id="current">Initializing...</div>
+	<div id="map_canvas" style="width:320px; height:350px"></div>
 
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//code.google.com/apis/gears/gears_init.js" type="text/javascript" charset="utf-8"></script>
+<script type="text/javascript" src="//maps.google.com/maps/api/js?sensor=false"></script>
 <script type="text/javascript">
-$()function(){
-	geofindMe();
-};
+initialize_map();
+initialize();
 
-function geoFindMe() {
-  var output = document.getElementById("out");
+function initialize_map()
+{
+    var myOptions = {
+	      zoom: 4,
+	      mapTypeControl: true,
+	      mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+	      navigationControl: true,
+	      navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+	      mapTypeId: google.maps.MapTypeId.ROADMAP      
+	    }	
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+function initialize()
+{
+	if(navigator.geolocation)
+	{
+		document.getElementById('current').innerHTML="Receiving...";
+		navigator.geolocation.getCurrentPosition(show_position,function(){document.getElementById('current').innerHTML="Couldn't get location"},{enableHighAccuracy:true});
+	}
+	else
+	{
+		document.getElementById('current').innerHTML="Functionality not available";
+	}
+}
 
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
+function show_position(p)
+{
+	document.getElementById('current').innerHTML="latitude="+p.coords.latitude.toFixed(2)+" longitude="+p.coords.longitude.toFixed(2);
+	var pos=new google.maps.LatLng(p.coords.latitude,p.coords.longitude);
+	map.setCenter(pos);
+	map.setZoom(14);
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+	var infowindow = new google.maps.InfoWindow({
+	    content: "<strong>yes</strong>"
+	});
 
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+	var marker = new google.maps.Marker({
+	    position: pos,
+	    map: map,
+	    title:"You are here"
+	});
 
-    var img = new Image();
-    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-    output.appendChild(img);
-  };
-
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-
-  output.innerHTML = "<p>Locating…</p>";
-
-  navigator.geolocation.getCurrentPosition(success, error);
+	google.maps.event.addListener(marker, 'click', function() {
+	  infowindow.open(map,marker);
+	});
+	
 }
 </script>
-<?php /*
-<script type="text/javascript" src="/assets/js/geoPosition.js" charset="utf-8"></script>
-<script type="text/javascript">
-    if(geoPosition.init()){  // Geolocation Initialisation
-        geoPosition.getCurrentPosition(success_callback,error_callback,{enableHighAccuracy:true});
-    }else{
-        // You cannot use Geolocation in this device
-		alert('You cannot use Geolocation in this device');
-    }
-    // geoPositionSimulator.init(); 
-
-    // p : geolocation object
-    function success_callback(p){
-		console.log(p);
-		$('body').html(p.coords.latitude + ' ' + p.coords.longitude);
-    }
-
-    function error_callback(p){
-        // p.message : error message
-    }
-</script>
-*/
-?>
 </body>
 </html>
  
