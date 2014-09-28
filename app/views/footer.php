509bc37effa21bf90370ae573bd3e7d907d03559
Simon Harrison <hypertexture@gmail.com>
2014-09-28 04:12:39 +0100

    No GPS handling

--- app/views/footer.php
+++ app/views/footer.php
@@ -9,7 +9,6 @@
 </div>
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
 <script src="/app/views/js/plugins.js"></script>
 <script src="/app/views/js/main.js"></script>
 <script src="/app/js/modernizr-2.6.2.min.js"></script>
