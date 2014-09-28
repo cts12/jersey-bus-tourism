a3605bfc0c2514784aef87a2da57c4849bd836ed
cts12 <charchris@race-nation.com>
2014-09-28 01:42:47 +0100

    looking at destination stuff

--- app/js/app.js
+++ app/js/app.js
@@ -140,3 +140,77 @@ function SortByDistance(a, b) {
 function setHeader(xhr) {
     xhr.setRequestHeader("Authorization", "Basic aGFja2F0aG9uZGVtbzpoYWNrYXRob25kZW1v");
 }
+
+function showRoutes(mylocation, destlocation) {
+
+    //my closest bus stops
+    //infor available are geo co-ordinates.
+    var my_close_stops = closeRouteStops(mylocation);
+
+
+    var dest_stops = closeRouteStops(destlocation);
+
+    //get our markers and the destination markers
+
+    for(var i = 0 ; i < my_close_stops.length ; i++){
+
+        var my_stop = my_close_stops[i];
+        var my_code = my_stop.Code;
+
+        for(var j = 0 ; j < dest_stops.length ; j++){
+
+            var dest_stop = dest_stops[j];
+            var dest_code = dest_stop.Code;
+
+
+        }
+    }
+
+
+
+    // google.maps.event.addListener(marker, 'click', (function (marker, i) {
+    //             return function () {
+    //                 infowindow.setContent('' + html2[i].Name + '<br>' + html2[i].distance.toFixed(2) + 'km');
+    //                 infowindow.open(map, marker);
+    //             }
+    //         })(marker, i));
+    //     }
+
+
+}
+
+function closeRouteStops(mylocation) {
+    var html = [];
+    $.getJSON("bus-stops.json", function (data) {
+
+        /* loop through array */
+        $.each(data, function (index, d) {
+            var dist = getDistanceInKm(myLocation.Latitude, myLocation.Longitude, d.Latitude, d.Longitude);
+            html.push({ Name: d.Name, Latitude: d.Latitude, Longitude: d.Longitude, distance: dist, d.Code });
+        });
+
+        var html2 = html.sort(SortByDistance);
+
+        // nearby Markers
+        // var markers = []
+        // for (i = 0; i < 5; i++) {
+
+        //     markers.push(marker = new google.maps.Marker({
+        //         position: new google.maps.LatLng(html2[i].Latitude, html2[i].Longitude),
+        //         map: map
+        //     }));
+
+        // }
+
+        // return markers;
+
+        return html2; 
+
+    }).error(function (jqXHR, textStatus, errorThrown) { /* assign handler */
+        /* alert(jqXHR.responseText) */
+        alert("error occurred!");
+    });
+
+
+
+}
--- bus-map.php
+++ bus-map.php
@@ -19,13 +19,42 @@ require 'app/views/header.php' ;
 	$(document).ready(function(){
 			
 		if(navigator.geolocation){
-			navigator.geolocation.getCurrentPosition(initialize,function(){alert('Couldn\'t find your location')},{enableHighAccuracy:true,timeout:10000});
+			navigator.geolocation.getCurrentPosition(initialize,errorhandler,{enableHighAccuracy:true,timeout:10000});
 		} else {
 			alert('Functionality not available');
+		}
+
+		function errorhandler(){
+
+			if(true){
+
+				var myLocation = {
+				// Latitude: user.coords.latitude.toFixed(5),
+				// Longitude: user.coords.longitude.toFixed(5)
+				Latitude: 49.1778315,
+				Longitude: -2.0810084
+			}
+			
+			var mapOptions = {
+				zoom: 16,
+				center: new google.maps.LatLng(49.1778315, -2.0810084),
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			  };
+
+			map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+		
+			myLocationMarker(myLocation);
+			//getLiveBus(myLocation);
+			getNearbyStopPoints(myLocation);
+
+			} else {
+				// function(){alert('Couldn\'t find your location')};
+			}
 		}
 		
 		function initialize(user) {	
 			
+	
 			var myLocation = {
 				Latitude: user.coords.latitude.toFixed(5),
 				Longitude: user.coords.longitude.toFixed(5)
@@ -33,7 +62,7 @@ require 'app/views/header.php' ;
 			
 			var mapOptions = {
 				zoom: 16,
-				center: new google.maps.LatLng(myLocation.Latitude, myLocation.Longitude),
+				center: new google.maps.LatLng(49.1778315, -2.0810084),
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			  };
 
