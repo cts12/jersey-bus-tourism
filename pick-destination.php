a5edd1c650f09e6fdc0c262ffe9fbca6c6d6e1f6
Robbie Andrews <robbie@beobserved.com>
2014-09-28 01:17:35 +0100

    Created pick-destination, added zoo logo and styling for that page

---
+++ pick-destination.php
@@ -0,0 +1,37 @@
+<?php
+$meta = array(
+	'title' => 'Get a Bus | Omnibus #hackjsy 2014',
+	'description' => '',
+	'keywords' => ''
+);
+$heading = 'PICK A DESTINATION';
+
+require 'app/views/header.php' ;
+?>
+ <!-- CONTENT SECTION -->
+            <div id="content-pick-destination">
+
+                <div class="destination-box ">
+                    <a href="/bus-map.php">
+                    <div class="home-icon">
+                        <img src="app/views/img/destination_icon.png" alt="bus stop icon" />
+                    <p>Town</p>
+                    </div>
+                    </a>
+                </div>
+
+                <div class="destination-box">
+                    
+                    <div class="home-icon">
+                         <img src="app/views/img/logo_zoo.png" alt="zoo logo" />
+                    <p>Durrell</p>
+                    </div>
+                    
+                </div>
+
+
+            </div>
+ <!-- END OF CONTENT SECTION -->
+
+<?php 
+require 'app/views/footer.php';
