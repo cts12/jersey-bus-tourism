ff42dd99e9bd25424c42be996376aaf45e6b604f
Simon Harrison <hypertexture@gmail.com>
2014-09-29 22:35:49 +0100

    Addition of GA + bug fix

--- info.php
+++ info.php
@@ -10,13 +10,16 @@ require 'app/views/header.php' ;
 ?>
  <!-- CONTENT SECTION -->
             <div id="content-info">
-                <p>OMNIBUS was built over 40 hours during the <a href="http://www.techtribes.je">TechTribes</a> <a href="http://www.twitter.com/@hackjsy">#HackJsy</a> Hackathon.</p> 
+                <p>OMNIBUS was built over 40 hours during the <a href="http://www.techtribes.je">TechTribes</a> <a href="http://www.twitter.com/@hackjsy">#HackJsy</a> Hackathon 2014.</p> 
                 <h1>TEAM</h1>
                 <p class="team-member">Simon</p>
                 <p class="team-member">Rui</p>
                 <p class="team-member">Sharkey</p>
                 <p class="team-member">Robbie</p>
                 <p class="team-member">Phil</p>
+				<br/>
+				<h2>Special thanks to:</h2>
+				<br/>
                 <p class="team-member">Ian</p>
                 <p class="team-member">Matt</p>
                 <p class="team-member">Rob</p>
