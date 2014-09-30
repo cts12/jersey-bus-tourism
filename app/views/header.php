e138a19c40d95d51eddccd35039be270f2d975d9
Simon Harrison <hypertexture@gmail.com>
2014-09-30 18:24:08 +0100

    Move GA to head

--- app/views/header.php
+++ app/views/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html/>
+<!DOCTYPE html>
 <head>
 	<meta charset="utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
@@ -7,13 +7,19 @@
 	<meta name="description" content="<?=$meta['description']?>"/>
 	<meta name="keywords" content="<?=$meta['keywords']?>"/>
 	<meta name="viewport" content="width=device-width, initial-scale=0.8, maximum-scale=1">
-     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<!-- <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"type="text/javascript"></script> -->
 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600' rel='stylesheet' type='text/css'>
-
 	<link rel="stylesheet" href="app/views/css/normalize.css">
 	<link rel="stylesheet" href="app/views/css/main.css">
+	
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-55272326-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 </head>
 <body>
 	<!--[if lt IE 7]>
